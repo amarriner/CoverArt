1a88bd30851e2df58df400aa2718d60a0f94e751
Aaron Marriner <amarriner@amarriner.com>
2014-08-21 15:52:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bot that mashes NES boxart together and tweets the results
+
+
---
+++ nes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Script that mashes NES boxart together and tweets them"""
+
+import gd
+import sys
+
+
+def main():
+   """Initial entry point"""
+   pass
+
+
+if __name__ == '__main__':
+   sys.exit(main())
